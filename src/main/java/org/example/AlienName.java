2ded29136ad4815e98799f791376441378e9f7a2
Ufuk Güler <ufukglr@yandex.com>
2020-06-24 15:02:41 +0300

    AlienName created -used for alian-

---
+++ src/main/java/org/example/AlienName.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class AlienName {
+    // column annotation can be setteble
+    private String fname;
+    private String mname;
+    private String lname;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+
+    @Override
+    public String toString() {
+        return "AlienName{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", mname='" + mname + '\'' +
+                '}';
+    }
+}
